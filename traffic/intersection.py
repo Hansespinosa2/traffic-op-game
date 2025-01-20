52a65761d533f8ed922ad8007221e58a6687ee80
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-01-20 12:50:20 -0500

    Write Data structures section of paper and start

---
+++ traffic/intersection.py
@@ -0,0 +1,71 @@
+from collections import deque
+class Intersection:
+    def __init__(self, type_):
+        self.type_ = type_
+
+class Roundabout(Intersection):
+    def __init__(self):
+        super().__init__(type_='Roundabout')
+        self.occupied_sections = []  # Tracks occupied sections (degrees)
+
+    def can_enter(self, vehicle_type):
+        if vehicle_type == 'Moped':
+            free_section = 60
+        elif vehicle_type == 'Bus':
+            free_section = 120
+        else:
+            free_section = 90
+
+        occupied_space = sum(section[1] - section[0] for section in self.occupied_sections)
+        return occupied_space + free_section <= 360
+
+    def enter(self, vehicle_type):
+        if self.can_enter(vehicle_type):
+            if vehicle_type == 'Moped':
+                section = (0, 60)
+            elif vehicle_type == 'Bus':
+                section = (0, 120)
+            else:
+                section = (0, 90)
+            self.occupied_sections.append(section)
+
+class StopSign(Intersection):
+    def __init__(self):
+        super().__init__(type_='StopSign')
+        self.queue = deque()
+
+    def add_to_queue(self, vehicle):
+        self.queue.append(vehicle)
+
+    def process_queue(self):
+        if self.queue:
+            return self.queue.popleft()
+
+class TrafficLight(Intersection):
+    def __init__(self, mode, param):
+        super().__init__(type_='TrafficLight')
+        self.mode = mode
+        self.param = param
+        self.time_elapsed = 0
+        self.car_queue = deque()
+        self.light_state = 'Red'
+
+    def update_light(self):
+        if self.mode == 'Timer':
+            if self.time_elapsed >= self.param:
+                self.light_state = 'Green' if self.light_state == 'Red' else 'Red'
+                self.time_elapsed = 0
+        elif self.mode == 'Sensor':
+            if len(self.car_queue) > self.param:
+                self.light_state = 'Green'
+            else:
+                self.light_state = 'Red'
+        elif self.mode == 'Sensor-Timer':
+            if len(self.car_queue) > self.param or self.time_elapsed >= self.param:
+                self.light_state = 'Green'
+                self.time_elapsed = 0
+            else:
+                self.light_state = 'Red'
+
+    def add_to_queue(self, vehicle):
+        self.car_queue.append(vehicle)
---
+++ traffic/vehicle.py
@@ -0,0 +1,27 @@
+# Base Vehicle class
+class Vehicle:
+    def __init__(self, max_speed, entrance, exit, paths):
+        self.max_speed = max_speed
+        self.entrance = entrance
+        self.exit = exit
+        self.paths = paths
+
+    def choose_path(self, traffic_counts):
+        """Choose the path with the smallest number of cars."""
+        return min(self.paths, key=lambda path: traffic_counts[path])
+
+# Car class
+class Car(Vehicle):
+    def __init__(self, entrance, exit, paths):
+        super().__init__(max_speed=60, entrance=entrance, exit=exit, paths=paths)
+
+# Moped class
+class Moped(Vehicle):
+    def __init__(self, entrance, exit, paths):
+        super().__init__(max_speed=45, entrance=entrance, exit=exit, paths=paths)
+
+# Bus class
+class Bus(Vehicle):
+    def __init__(self, entrance, exit, path, entrance_time):
+        super().__init__(max_speed=40, entrance=entrance, exit=exit, paths=[path])
+        self.entrance_time = entrance_time
